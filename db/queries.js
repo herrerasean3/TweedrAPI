7ea622b0513f95a4db9b2980df0de99a03db726e
herrerasean3 <herrerasean3@gmail.com>
2017-05-22 10:26:45 -0400

    Commented query code

--- db/queries.js
+++ db/queries.js
@@ -2,6 +2,23 @@ let pgp = require('pg-promise')();
 let connString = process.env.DATABASE_URL;
 let db = pgp(connString);
 
+/*
+Tweedr uses a single table database. To this end, all relevant data is contained in the table "tweed".
+The table is organized like so:
+
+tweed_id | username | tweed_content | tweed_timestamp | reply_id
+
+tweed_id, tweed_timestamp, and reply_id are automatically resolved by the code, and are by default, impossible to edit.
+This serves to ensure preservation of some post data, even if the username and content have been altered.
+
+username is a VARCHAR field, and currently is uncapped. Please avoid colossal usernames.
+tweed_content is also a VARCHAR field, but limited to 120 characters in the spirit of Twitter.
+*/
+
+// Called whenever the index recieves a GET request.
+// Grabs the entire tweed table, save for the placeholder post.
+// Sorts the posts by timestamp, then ID.
+
 function readAllPosts(req, res, next) {
 	db.any('SELECT * FROM tweed WHERE tweed_id > 1 ORDER BY tweed_timestamp DESC, tweed_id DESC')
 		.then(function(data) {
@@ -15,6 +32,12 @@ function readAllPosts(req, res, next) {
 			return next(err);
 		});
 }
+
+// Called whenever '/reply/:id' recieves a GET request.
+// Using a db.task, two database queries are made in a batch.
+// "OP" contains the post to which the parameter ID is pointing to.
+// "replies" contains all posts with a reply_id matching the tweed_id of the OP.
+// targetID is present at several points in the code, ensuring that there is no way for users to touch the placeholder post.
 
 function getPostReplies(req, res, next) {
 	let targetID = parseInt(req.params.id) + 1;
@@ -41,6 +64,10 @@ function getPostReplies(req, res, next) {
 		});
 }
 
+// Called when a POST request is sent to the index.
+// Takes "username" and "tweed_content" as values for the request.
+// Timestamp is generated automatically, and for new posts, reply ID points to the placeholder post.
+
 function submitPost(req, res, next) {
 	db.none('INSERT INTO tweed(username,tweed_content,tweed_timestamp,reply_id)' + 
 		'values(${username}, ${tweed_content}, CURRENT_TIMESTAMP, 1)',
@@ -56,6 +83,11 @@ function submitPost(req, res, next) {
       return next(err);
     });
 }
+
+// Called when a POST request is sent to '/reply/:id'.
+// Takes the same values as the previous function.
+// Uses the id from the URL to determine which post is being replied to and set a replyID accordingly.
+// This is done via the targetID variable.
 
 function submitReply(req, res, next) {
 	console.log(req.body)
@@ -79,6 +111,9 @@ function submitReply(req, res, next) {
     });
 }
 
+// Called when a DELETE request request is sent to '/reply/:id'.
+// Only deletes specific posts, so as to avoid easy bulk post wiping.
+
 function deletePost(req, res, next) {
 	let targetID = parseInt(req.params.id) + 1;
 	if (targetID < 2) {
@@ -97,6 +132,9 @@ function deletePost(req, res, next) {
       return next(err);
     });
 }
+
+// Called when a PUT request is sent to '/reply/:id'.
+// Uses identical arguments to the POST functions up above.
 
 function editPost(req, res, next) {
 	let targetID = parseInt(req.params.id) + 1;
